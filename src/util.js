2aebab8500f1356b6cdf5aeb081475d1d71a45d2
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2022-04-07 23:02:26 -0600

    styles Done

---
+++ src/util.js
@@ -0,0 +1,16 @@
+import {v4 as uuidv4}from "uuid"
+
+
+function chillHop() {
+    return [
+        {
+            name:"Beaver creek",
+            cover:'',
+            audio:"",
+            color:['#205950',"#2ab3bf"],
+            id:uuidv4(),
+            active:true,
+
+        }
+    ]
+}
